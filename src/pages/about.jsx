cebeb18b7a9dd686c71a032a8ae0ffe9c6f92eb5
chris <cupidodice042014@hotmail.com>
2021-07-31 22:59:51 -0300

    aprendiondo a usar gatsby

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const about = () => {
+  return (
+    <Layout pageTitle="About">
+      <h2>christopher</h2>
+    </Layout>
+  );
+};
+
+export default about;
